3c90afe48f6f8b1777ad76e656003f1e62cdb548
YAHIR RAMOS <yahiramoslinks@hotmail.com>
2022-12-30 00:26:35 -0600

    proyecto inicializado y primeros styled components creados

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled"
+import criptoImage from './img/imagen-criptos.png'
+
+
+//los styled components se declaran antes de el componente principal
+//el nombre de estos componentes se define igual en mayusculas
+
+const Container=styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width:90%;
+  @media(min-width:992px){
+    display:grid;
+    grid-template-columns:repeat(2,1fr);
+    column-gap:2rem;
+  }
+`
+const Heading=styled.h1`
+  font-family:'Lato',sans-serif;
+  color:#fff;
+  text-align:center;
+  font-weight:700;
+  margin-top:80px;
+  margin-bottom:50px;
+  font-size:40px;
+  &::after{
+    content:'';
+    width:250px;
+    height:10px;
+    background-color:#66A2FE;
+    display:block;
+    margin:10px auto 0 auto;
+  }
+`
+const Image=styled.img`
+  max-width:400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+const App = () => {
+  return (
+
+    <Container>
+      <Image
+        src={criptoImage}
+        alt='cripto-image'
+      />
+      <Heading>Cotizador de precios de Criptomonedas</Heading>
+    </Container>
+  )
+}
+
+export default App
